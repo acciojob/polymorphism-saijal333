adab9f6fd416cff7aa9e594ef62c7090f3e31960
saijal333 <saijal.shrivastava333@gmail.com>
2023-08-14 10:33:43 +0530

    hello

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,28 +1,26 @@
 package com.driver;
 
 public class Main {
-public static void main(String[] args){
-    Product p = new Product();
-    int result1 = p.product(5,3);
-    System.out.println("Product of two integers: " + result1);
-
-    int result2 = p.product(2,4,3);
-    System.out.println("Product of three integers: " + result2);
-
-    double result3 = p.product(2.5, 3.0);
-    System.out.println("Product of two doubles: " + result3);
-
-     }
+    static class Product{
+        public int product(int x, int y){
+            return x*y;
+        }
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+        public double product(double x, double y){
+            return x*y;
+        }
     }
+    public static void main(String[] args) {
+        Product p = new Product();
+        int result1 = p.product(5, 3);
+        System.out.println("Product result 2 parameters: " + result1);
 
-    class Product{
-    public int product(int x, int y){
-        return x*y;
-    }
-    public int product(int x, int y, int z){
-        return x*y*z;
-    }
-    public double product(double x, double y){
-        return x*y;
+        int result2 = p.product(2,4,5);
+        System.out.println("Product result 3 parameters: " + result2);
+
+        double result3 = p.product(2.5, 3.5);
+        System.out.println("Product result(double): " + result3);
     }
     }
