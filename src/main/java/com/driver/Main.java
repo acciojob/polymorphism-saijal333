a6a7a0a775ed0b9f3577985105e08b2d9ffb6378
saijal333 <saijal.shrivastava333@gmail.com>
2023-08-15 20:16:18 +0530

    hehe

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,16 +3,17 @@ package com.driver;
 
 class Product {
     // Task 3: Method with two int parameters
-    static int product(int x, int y) {
+    public static int product(int x, int y)
+    {
         return x*y;
     }
 
-    static int product(int x, int y, int z)
+    public static int product(int x, int y, int z)
     {
         return x * y * z;
     }
 
-    static double product(double x, double y) {
+    public static double product(double x, double y) {
         return x * y;
     }
 }
@@ -21,11 +22,11 @@ public class Main {
 
         Product p = new Product();
 
-        System.out.println(p.product(2,3));
+        System.out.println(p.product(2, 3));
 
         System.out.println(p.product(2, 3, 4));
 
-        System.out.println(p.product(2.5, 3.0));
+        System.out.println(p.product(2.5, 3.2));
 
     }
 }
